3f21c6c53f7387ceeda852123e95c946ee954c4f
iamarale <ismailarales@gmail.com>
2024-03-20 11:21:26 -0500

    added convex

---
+++ components/Projects.tsx
@@ -0,0 +1,10 @@
+export default function Projects() {
+    return (
+        <div className="p-4 border-2">
+            <h1>Projects</h1>
+            <p>Coming soon...</p>
+
+        </div>
+    )
+}
+
